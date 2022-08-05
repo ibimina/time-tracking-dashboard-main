74d210bdcb3b5fe9f340af6e57edb52d21818653
ibimina <96787210+ibimina@users.noreply.github.com>
2022-08-05 11:09:25 +0100

    update comments

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+//get the container of each time frame using element Id
+//fetch data from json file
+//insert the data gotten from the json file into index.html
+
 let day = document.querySelector("#daily");
 let week = document.querySelector("#weekly");
 let month = document.querySelector("#monthly");
+
 async function data() {
   try {
     const request = await fetch("data.json");
@@ -24,13 +29,13 @@ data().then((res) => {
       past
     );
     past = "Last week";
-    weekly.innerHTML += html(
+    week.innerHTML += html(
       title,
       timeframes.weekly.current,
       timeframes.weekly.previous,
       wk
     );
-    monthly.innerHTML += html(
+    month.innerHTML += html(
       title,
       timeframes.monthly.current,
       timeframes.monthly.previous,
@@ -38,6 +43,8 @@ data().then((res) => {
     );
   });
 });
+//html format for each activity 
+
 const html = (title, current, previous, past) => {
   let template = ` <section class="activity">
 
@@ -58,13 +65,22 @@ const html = (title, current, previous, past) => {
       </sec>`;
   return template;
 };
+//to display one timeframe at a time
+//select all timeframe container 
+//set them to display none
+//remove the display none from the first container to show visiblity
+//create  a function that shows one timeframe when daily or weekly or monthly link is clicked
 
 const showTimeframe = document.querySelectorAll(".tm-con");
+
 const hiddenTimeFrame = () => {
   showTimeframe.forEach((timeframe) => timeframe.classList.add("display"));
 };
+
 hiddenTimeFrame();
+
 showTimeframe[0].classList.remove("display");
+
 const ul = document.querySelector("ul");
 
 const links = document.querySelectorAll(".nav");
